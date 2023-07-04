32f9cf4e339412764b65727bcc49f61e2cba1fa1
GHeeJeon <jhjeon_99@naver.com>
2023-07-04 10:10:52 +0900

    react-resize button

--- js-react/src/App.js
+++ js-react/src/App.js
@@ -112,13 +112,13 @@ function App() {
     return (
         <div className="App bg-blue-100 min-h-screen flex flex-col justify-center items-center space-y-5">
             <div>
-                <h1 className="text-4xl text-blue-500">오늘의 일기</h1>
+                <h1 className="text-4xl text-blue-600">오늘의 일기</h1>
             </div>
             <div className="input-section border-solid border-2 border-sky-400 py-4 px-8 rounded-2xl">
                 <form className="input-form" onSubmit={handleFormSubmit}>
-                    <input placeholder={'Enter date'} type="date" value={dateInput} onChange={handleDateChange}/>
-                    <input placeholder={'Enter text'} type="text" value={textInput} onChange={handleTextChange}/>
-                    <button type="submit">완료</button>
+                    <input className="py-2 px-4 border-2 border-blue-400 rounded-md"  placeholder={'Enter date'} type="date" value={dateInput} onChange={handleDateChange}/>
+                    <input className="py-2 px-4 border-2 border-blue-400 rounded-md" placeholder={'Enter text'} type="text" value={textInput} onChange={handleTextChange}/>
+                    <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="submit">완료</button>
                 </form>
                 {showSnackbar === 'success' &&
                     <div className={`bg-green-500/75 snackbar ${showSnackbar ? 'show' : ''}`}>항목을 추가했어요!</div>}
@@ -129,25 +129,23 @@ function App() {
             </div>
             <div className="list-section w-9/12">
                 {diaries.map(diary => (
-                    <div key={diary.id} className="diary-item bg-transparent border-solid border-2 border-sky-300 m-4 py-4 px-8 rounded-2xl justify-between">
+                    <div key={diary.id} className="diary-item bg-transparent border-solid border-2 border-sky-300 m-4 py-4 px-8 rounded-2xl">
                         {editingDiaryId === diary.id ? (
-                            <form className="edit-form w-full justify-between" onSubmit={handleEditFormSubmit}>
-                                <div>
-                                <input type="date" value={editDateInput} onChange={handleEditDateChange}/>
-                                <input type="text" value={editTextInput} onChange={handleEditTextChange}/>
+                            <>
+                                <div className="diary-date"><input className="py-1 px-2 border-2 border-blue-400 rounded-md" type="date" value={editDateInput} onChange={handleEditDateChange}/></div>
+                                <div className="diary-text"><input className="py-1 px-2 border-2 border-blue-400 rounded-md" type="text" value={editTextInput} onChange={handleEditTextChange}/></div>
+                                <div className="diary-action justify-between w-full">
+                                    <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="submit" onClick={handleEditFormSubmit}>저장</button>
+                                    <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="button" onClick={cancelEditing}>취소</button>
                                 </div>
-                                <div className="diary-action justify-end">
-                                    <button type="submit">저장</button>
-                                    <button type="button" onClick={cancelEditing}>취소</button>
-                                </div>
-                            </form>
+                            </>
                         ) : (
                             <>
                                 <div className="diary-date">{diary.date}</div>
                                 <div className="diary-text">{diary.text}</div>
-                                <div className="diary-action">
-                                    <button onClick={() => editDiary(diary.id)}>수정</button>
-                                    <button onClick={() => deleteDiary(diary.id)}>삭제</button>
+                                <div className="diary-action justify-between w-full">
+                                    <button className="py-1 px-2 bg-blue-200 border-2 border-blue-300 rounded-md" onClick={() => editDiary(diary.id)}>수정</button>
+                                    <button className="py-1 px-2 bg-blue-200 border-2 border-blue-300 rounded-md" onClick={() => deleteDiary(diary.id)}>삭제</button>
                                 </div>
                             </>
                         )}
