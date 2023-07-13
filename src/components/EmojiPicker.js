9f40fe5e178821326a834726f2d93a783fe74ca2
GHeeJeon <jhjeon_99@naver.com>
2023-07-14 03:42:54 +0900

    react-add comment

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Picker from '@emoji-mart/react';
 
 function EmojiPicker({ showEmojiPicker, emojiPickerRef, onEmojiSelect }) {
+    // ref={emojiPickerRef} -> emojiPickerRef.current
     return showEmojiPicker ? (
         <div className="absolute top-full z-10" ref={emojiPickerRef}>
             <Picker onEmojiClick={onEmojiSelect}/>
